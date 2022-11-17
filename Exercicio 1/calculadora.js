01498f4c917d03bb250f90aa03bf699ba0f9d3e3
warleymts <warlinpb@outlook.com>
2022-11-17 15:46:21 -0300

    Insert Exercício 1

---
+++ Exercicio 1/calculadora.js
@@ -0,0 +1,11 @@
+let firstNumber = Number(prompt("Insira um número de 0 a 99"));
+
+let operation = prompt("Insira uma operação: +, -, *, /");
+
+let secondNumber = Number(prompt("Insira outro número de 0 a 99"));
+
+
+if ((operation === "+")) alert(firstNumber + secondNumber);
+else if ((operation === "-")) alert(firstNumber - secondNumber);
+else if ((operation === "*")) alert(firstNumber * secondNumber);
+else if ((operation === "/")) alert(firstNumber / secondNumber);
