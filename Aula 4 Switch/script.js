2da087d5b57695e06642684110890791fbb7c0dc
Warley Martins <warlinpb@Outlook.com>
2023-02-02 16:24:23 -0300

    add aula 4

---
+++ Aula 4 Switch/script.js
@@ -0,0 +1,15 @@
+const resultado = prompt("Escolha um resultado: a, b ou c.");
+
+switch (resultado) {
+  case "a":
+    alert("O resultado é A");
+    break;
+  case "b":
+    alert("O resultado é B");
+    break;
+  case "c":
+    alert("O resultado é C");
+    break;
+  default:
+    alert("Finalizando");
+}
